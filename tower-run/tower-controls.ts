f691feb02e8bd4499cf6e2fd24db5fcc2e17aa0e
Herman Rademeyer <98677997+maans-dev@users.noreply.github.com>
2022-08-22 16:41:11 +0200

    Update tower-controls.ts

--- tower-run/tower-controls.ts
+++ tower-run/tower-controls.ts
@@ -1,39 +1,48 @@
-import {GamepadControls, DropJoystickControls} from "../../api/lib/ControlTypes.js";
-import {AsciiMapping} from "../../api/lib/AsciiMapping.js";
-import {DosEmulatorImpl} from "../../api/lib/DosEmulatorImpl.js";
-import {GamepadMapping} from "../../api/lib/GamepadMapping.js";
+import {
+  GamepadControls,
+  DropJoystickControls,
+} from '../../api/lib/ControlTypes.js';
+import { AsciiMapping } from '../../api/lib/AsciiMapping.js';
+import { DosEmulatorImpl } from '../../api/lib/DosEmulatorImpl.js';
+import { GamepadMapping } from '../../api/lib/GamepadMapping.js';
 
 /**
- * ### DIGGER [CONTROLS] ###
+ * ### TOWER [CONTROLS] ###
  * --------------------------
  **
- * DIGGER game controls (DropJoystick and ScreenControls)
+ *  game controls (DropJoystick and ScreenControls)
  */
-export const buttons:GamepadControls[] & DropJoystickControls[] = [
-    {
-        'name': 'Fire',
-        'iconPath': './icons/fire.svg',
-        'hexColour' : 0xff0000,
-        'pressFunction': (dosGame) => dosGame.pressKey(AsciiMapping.F1, true),
-        'releaseFunction': (dosGame) => dosGame.pressKey(AsciiMapping.F1, false),
-        'gamepadButton': GamepadMapping.A
-    },
-    {
-        'name': 'Up',
-        'pressFunction': (dosGame:DosEmulatorImpl) => dosGame.pressKey(AsciiMapping.ARROW_UP, true),
-        'releaseFunction': (dosGame:DosEmulatorImpl) => dosGame.pressKey(AsciiMapping.ARROW_UP, false),
-        'gamepadButton': GamepadMapping.DPAD_UP
-    },
-    {
-        'name': 'Left',
-        'pressFunction': (dosGame:DosEmulatorImpl) => dosGame.pressKey(AsciiMapping.ARROW_LEFT, true),
-        'releaseFunction': (dosGame:DosEmulatorImpl) => dosGame.pressKey(AsciiMapping.ARROW_LEFT, false),
-        'gamepadButton': GamepadMapping.DPAD_LEFT
-    },
-    {
-        'name': 'Right',
-        'pressFunction': (dosGame:DosEmulatorImpl) => dosGame.pressKey(AsciiMapping.ARROW_RIGHT, true),
-        'releaseFunction': (dosGame:DosEmulatorImpl) => dosGame.pressKey(AsciiMapping.ARROW_RIGHT, false),
-        'gamepadButton': GamepadMapping.DPAD_RIGHT
-    }
-]
+export const buttons: GamepadControls[] & DropJoystickControls[] = [
+  {
+    name: 'Fire',
+    iconPath: './icons/fire.svg',
+    hexColour: 0xff0000,
+    pressFunction: (dosGame) => dosGame.pressKey(AsciiMapping.SPACE, true),
+    releaseFunction: (dosGame) => dosGame.pressKey(AsciiMapping.SPACE, false),
+    gamepadButton: GamepadMapping.A,
+  },
+  {
+    name: 'Up',
+    pressFunction: (dosGame: DosEmulatorImpl) =>
+      dosGame.pressKey(AsciiMapping.ARROW_UP, true),
+    releaseFunction: (dosGame: DosEmulatorImpl) =>
+      dosGame.pressKey(AsciiMapping.ARROW_UP, false),
+    gamepadButton: GamepadMapping.DPAD_UP,
+  },
+  {
+    name: 'Left',
+    pressFunction: (dosGame: DosEmulatorImpl) =>
+      dosGame.pressKey(AsciiMapping.ARROW_LEFT, true),
+    releaseFunction: (dosGame: DosEmulatorImpl) =>
+      dosGame.pressKey(AsciiMapping.ARROW_LEFT, false),
+    gamepadButton: GamepadMapping.DPAD_LEFT,
+  },
+  {
+    name: 'Right',
+    pressFunction: (dosGame: DosEmulatorImpl) =>
+      dosGame.pressKey(AsciiMapping.ARROW_RIGHT, true),
+    releaseFunction: (dosGame: DosEmulatorImpl) =>
+      dosGame.pressKey(AsciiMapping.ARROW_RIGHT, false),
+    gamepadButton: GamepadMapping.DPAD_RIGHT,
+  },
+];
